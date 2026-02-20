cb0d925d14238d9b6712754cab2db1d04037c406
Mobiasson <mikaeltobiasson@hotmail.com>
2026-02-20 10:38:51 +0100

    Added first version of game just to get started

---
+++ js/Game.js
@@ -0,0 +1,24 @@
+import { createDeck, getShuffledDeck, shuffle, getCard } from './Deck.js';
+
+const playerHandContainer = document.getElementById('your-hand');
+const playerHand = []
+const deck = []
+
+function displayPlayerCards(card, container) {
+  const img = document.createElement('img');
+  img.src = card.image;
+  container.appendChild(img);
+}
+
+function updateCards() {
+  playerHandContainer.innerHTML = '';
+  playerHand.forEach(card => displayPlayerCards(card, playerHandContainer));
+
+}
+
+function startNewGame() {
+  playerHand = [deck.pop(), deck.pop()];
+  updateCards();  
+}
+
+startNewGame();
